dfc2c21fb921fbb850eba571384f0b1012c9d0d8
Shaan <Shaan@admins-MacBook-Pro.local>
2019-09-21 17:57:56 -0700

    finished first page of tutorial

--- dog.py
+++ dog.py
@@ -1,5 +1,8 @@
-
-my_dog = Dog("Rex", "SuperDog")
-my_dog.breed = 
-print(my_dog.breed)
-
+class Dog:
+    # Required properties are defined inside the __init__ constructor method
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+    # Methods are defined as their own named functions inside the class
+    def bark(self):
+        print("Woof!")
--- my-dogs.py
+++ my-dogs.py
@@ -1,10 +1,7 @@
 import dog
 
-class Dog:
-    # Required properties are defined inside the __init__ constructor method
-    def __init__(self, name, breed):
-        self.name = name
-        self.breed = breed
-    # Methods are defined as their own named functions inside the class
-    def bark(self):
-        print("Woof!")
+
+my_dog = dog.Dog("Rex", "SuperDog")
+my_dog.bark()
+my_other_dog = dog.Dog("Annie", "SuperDog")
+print(my_other_dog.name)
