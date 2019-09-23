74fdba745b5ed5346baaab76163218e6dbb6c5f0
Shaan <Shaan@admins-MacBook-Pro.local>
2019-09-23 14:34:57 -0700

    finished super hero dueler

--- superheroes.py
+++ superheroes.py
@@ -9,7 +9,7 @@ class Ability:
         self.attack_strength = attack_strength
 
     def attack (self):
-        return random.randint(self.attack_strength, 100)
+        return random.randint(0, self.attack_strength)
         
 
 
@@ -22,10 +22,10 @@ class Armor:
         return block
 
 class Hero:
-    def __init__ (self, name, starting_health, current_health):
+    def __init__ (self, name, starting_health = 100):
         self.name = name
         self.starting_health = starting_health
-        self.current_health = current_health
+        self.current_health = 100
         self.abilities = list()
         self.armors = list()
 
@@ -55,14 +55,29 @@ class Hero:
         else:
             return False
 
-    def fight (self):
-        pass
+    def fight (self, opponent):
+        if len(self.abilities) == 0 and len(opponent.abilities):
+            return print("Draw!")
+        while self.is_alive() and opponent.is_alive():
+            self.take_damage(opponent.attack())
+            opponent.take_damage(self.attack())
+        if self.is_alive() == False:
+            return print(f"{opponent.name} won!")
+        else:
+            return print(f"{self.name} won!")
 
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    hero = Hero("Grace Hopper", 200, 200)
-    shield = Armor("Shield", 50)
-    hero.add_armor(shield)
-    hero.take_damage(50)
-    print(hero.current_health)
+    hero1 = Hero("Wonder Woman", 200)
+    hero2 = Hero("Dumbledore", 200)
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
+
