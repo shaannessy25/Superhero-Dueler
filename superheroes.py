e3a1d036cfefef7e6df659f8f989210ce067670d
Shaan <Shaan@admins-MacBook-Pro.local>
2019-09-29 19:56:57 -0700

    finishing 4th page of tutorial

--- superheroes.py
+++ superheroes.py
@@ -25,18 +25,30 @@ class Hero:
     def __init__ (self, name, starting_health = 100):
         self.name = name
         self.starting_health = starting_health
-        self.current_health = 100
+        self.current_health = starting_health
         self.abilities = list()
         self.armors = list()
-
+        self.deaths = 0
+        self.kills = 0
     def add_ability (self, ability):
         self.abilities.append(ability)
+    
+    def add_weapon(self, weapon):
+        self.abilities.append(weapon)
 
     def attack (self):
         damage_total = 0
         for ability in self.abilities:
             damage_total += ability.attack()
         return damage_total    
+
+    def add_kill(self, num_kills):
+        self.kills += num_kills
+        return self.kills
+    
+    def add_deaths(self, num_deaths):
+        num_deaths += self.deaths
+        return num_deaths
 
     def defend (self):
         block_total = 0
@@ -62,9 +74,14 @@ class Hero:
         while self.is_alive() and opponent.is_alive():
             self.take_damage(opponent.attack())
             opponent.take_damage(self.attack())
+
         if self.is_alive() == False:
+            self.add_deaths(1)
+            opponent.add_kill(1)
             return print(f"{opponent.name} won!")
         else:
+            self.add_kill(1)
+            opponent.add_deaths(1)
             return print(f"{self.name} won!")
 
 class Team:
@@ -85,6 +102,21 @@ class Team:
     def view_all_heroes(self):
         for heroes in self.heroes:
             print(heroes.name)
+
+    def attack(self, other_team):
+        hero1 = self.heroes[random.randint(0, (len(self.heroes))-1)]
+        hero2 = other_team.heroes[random.randint(0, (len(other_team.heroes))-1)]
+        hero1.fight(hero2)
+
+    def revive_heroes(self, health = 100):
+        for hero in self.heroes:
+            hero.current_health = health
+            
+        
+
+    def stats(self):
+        for hero in self.heroes:
+            print(f" Hero: {hero.name} Kills: {hero.kills} Deaths: {hero.deaths} KD Ratio: {hero.kills/hero.deaths}")
 
 def test_hero_fight():
     hero1 = Hero("Wonder Woman", 200)
@@ -110,6 +142,6 @@ def test_hero_fight():
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    for count in range(21):
+    for count in range(30):
         winner = test_hero_fight()
         print(winner)
